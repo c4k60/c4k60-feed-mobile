aaf2fcb91b59d5d7a9a759b0b97246a486ed4451
Duong Tung Anh <duongtunganh2111@gmail.com>
2021-03-11 20:15:08 +0700

    ok

--- index.php
+++ index.php
@@ -6,3 +6,33 @@ if (!isset($_SESSION['loggedin'])) {
   header('Location: /login');
 }
 ?>
+<!doctype html>
+<html>
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, maximum-scale=1">
+	  <link rel="icon" type="image/png" href="/c4k60.png">
+    <!-- Style -->
+    <link rel="stylesheet" href="css/style.css">
+    <script src="https://kit.fontawesome.com/5468db3c8c.js" crossorigin="anonymous"></script>
+    <title>C4K60 Feed</title>
+  </head>
+  <body style="background-color: rgb(218, 221, 225);">
+  	<div class="upper_search_box">
+  		<i class="fas fa-user-circle" id="profile_icon"></i>
+  		<div class="search_box">
+        <i class="fas fa-search" id="search_icon"></i>
+      Tìm kiếm
+      </div>
+  		<i class="fas fa-cog" id="setting_icon"></i>
+  	</div>
+    <div class="navigation_bar">
+      <img src="/assets/newsfeed.png" class="newsfeed_icon active">
+
+    </div>
+  	<div class="content">
+
+  	</div>
+  </body>
+</html>
