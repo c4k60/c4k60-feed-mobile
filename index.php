42bdfd45ba68cb7ee2892ec7800c37935c466b80
Duong Tung Anh <duongtunganh2111@gmail.com>
2021-03-12 18:23:14 +0700

    tunna

--- index.php
+++ index.php
@@ -28,11 +28,36 @@ if (!isset($_SESSION['loggedin'])) {
   		<i class="fas fa-cog" id="setting_icon"></i>
   	</div>
     <div class="navigation_bar">
+      <div class="newsfeed_icon">
     	<i class="fas fa-home newsfeed_icon active"></i>
+      <div class="indicator"></div>
+      </div>
     	<i class="fas fa-user-friends friends_icon"></i>
     	<i class="fas fa-comment chat_icon"></i>
     	<i class="fas fa-bell noti_icon"></i>
     	<i class="fas fa-bars menu_icon"></i>
+    </div>
+    <div class="what_do_you_think">
+      <img src="<?php echo $_SESSION['profile_pic'] ?>" class="profile_pic_wdyt">
+      <div class="wdyt_box">Bạn đang nghĩ gì?</div>
+      <div class="wdyt_photo_icon_area">
+        <i class="far fa-images wdyt_photo_icon"></i>
+        <div class="wdyt_photo_text">Hình ảnh</div>
+      </div>
+    </div>
+    <div class="story_area">
+      <div class="story_header">
+        <b class="bold_text">Tin</b>
+        <div class="story_archive">
+          <div class="story_archive_icon"></div>
+        Kho lưu trữ tin của bạn
+        </div>
+      </div>
+      <div class="story_preview">
+        <div>
+          <img class="profpic" src="<?php echo $_SESSION['profile_pic'] ?>">
+        </div>
+      </div>
     </div>
   	<div class="content">
 
