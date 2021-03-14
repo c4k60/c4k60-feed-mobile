73273f50d2b6a0af7d2f37ebc206421d59c17b87
Duong Tung Anh <duongtunganh2111@gmail.com>
2021-03-14 22:40:18 +0700

    ok

--- index.php
+++ index.php
@@ -45,22 +45,75 @@ if (!isset($_SESSION['loggedin'])) {
         <div class="wdyt_photo_text">Hình ảnh</div>
       </div>
     </div>
-    <div class="story_area">
-      <div class="story_header">
+    <div class="story_header">
         <b class="bold_text">Tin</b>
         <div class="story_archive">
-          <div class="story_archive_icon"></div>
-        Kho lưu trữ tin của bạn
-        </div>
-      </div>
-      <div class="story_preview">
-        <div>
-          <img class="profpic" src="<?php echo $_SESSION['profile_pic'] ?>">
-        </div>
-      </div>
+          	<div class="story_archive_icon"></div>
+        	Kho lưu trữ của bạn
+    	</div>
     </div>
-  	<div class="content">
-
+    <div class="story_area">
+      <div class="story_preview">
+          	<img class="profpic" src="<?php echo $_SESSION['profile_pic'] ?>">
+          	<div class="plus_background">
+          		<div class="plus_icon"></div>
+        	</div>
+        	<div class="add_to_story_area">
+        		<div class="add_to_story_text_area">
+        			<div>Thêm vào<br>tin</div>
+        		</div>
+        	</div>
+        	<div class="story owl-carousel owl-theme" style="position: relative;">
+  				<div class="story-item item ninebysixteen" style="background-image:url(https://4.img-dpreview.com/files/p/E~TS590x0~articles/3925134721/0266554465.jpeg);"> 
+   					<div class="rounded1"></div>
+    				<span>Dương Tùng Anh<span>
+    			</div>
+  				<div class="story-item item ninebysixteen" style="background-image:url(https://petapixel.com/assets/uploads/2012/02/sample1_mini.jpg);"> 
+   	 				<div class="rounded2"></div>
+    				<span>Hoàng Phát<span> 
+				</div>
+				<div class="story-item item ninebysixteen" style="background-image:url(/images/tunganhhai.jpg);"> 
+   	 				<div class="rounded3"></div>
+    				<span>Nguyễn Đặng Hải<span> 
+				</div>
+				<div class="story-item item ninebysixteen" style="background-image:url(/images/duongthaovabo.jpg);"> 
+   	 				<div class="rounded4"></div>
+    				<span>Dương Phương T...<span> 
+				</div>
+			</div>
+      </div>
+   	</div>
+  	<div class="newsfeed">
+  		<div id="post1" class="newsfeed_post">
+  			<header class="nf_post_header_area">
+				<div class="nf_post_avatar_area">
+					<a href="/tunnaduong">
+						<i class="nf_post_avatar img" role="img" style="background: #d8dce6 url(/images/tunna.jpg) no-repeat center;background-size: 100% 100%;width: 40px;height: 40px;"></i>
+					</a>
+				</div>
+				<div class="nf_post_header_info">
+					<a href="/tunnaduong" class="nf_post_name">Dương Tùng Anh</a><br>
+					<a href="/post.php?id=1" class="nf_post_time">1 giờ</a>
+					<span class="dot"> · </span>
+					<span class="audience_icon"><i class="fas fa-globe-americas"></i></span>
+				</div>
+  			</header>
+  			<div class="nf_post_body_area">
+  				<div class="nf_post_caption">
+  					<p>Xin chào các cháu đã quay trở lại kênh của bà tân vê lốc</p>
+  				</div>
+  				<div class="nf_post_image_area">
+  					<a href="/post"></a>
+  					<img src="/images/batan.jpg" class="nf_post_image">
+  				</div>
+  			</div>
+  			<footer class="nf_post_reaction">
+  				<hr>
+  				Thích
+  				Bình luận
+  				<hr>
+  			</footer>
+  		</div>
   	</div>
   </body>
 </html>
